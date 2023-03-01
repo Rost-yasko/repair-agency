d0e35f8af290698c3290b5927b0407c0318785f6
Rostyslav <105812263+Rost-yasko@users.noreply.github.com>
2023-03-01 15:24:35 +0200

    add connection pool and refactoring (#3)

---
+++ src/main/java/db/connection/PoolConnection.java
@@ -0,0 +1,41 @@
+package db.connection;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class PoolConnection implements ConnectionBuilder {
+    private final BasicDataSource ds = new BasicDataSource();
+
+    public PoolConnection() {
+        Properties property = new Properties();
+
+        try {
+            FileInputStream fis = new FileInputStream("src/main/resources/application.properties");
+            property.load(fis);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read properties file");
+        }
+
+        ds.setUrl(property.getProperty("db.url"));
+        ds.setUsername(property.getProperty("db.login"));
+        ds.setPassword(property.getProperty("db.password"));
+        ds.setMinIdle(5);
+        ds.setMaxIdle(10);
+        ds.setMaxOpenPreparedStatements(100);
+    }
+
+    public Connection getConnection() {
+        try {
+            return ds.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't connect to the DB");
+        }
+    }
+
+
+}
