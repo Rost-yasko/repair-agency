5c1edb13895c8cad7bcbd24f887e7fb98a64e734
Rostyslav <rostyasko@gmail.com>
2023-03-05 18:26:14 +0200

    Create DAO layer for database operations with User

---
+++ src/main/java/dao/IUserDao.java
@@ -0,0 +1,16 @@
+package dao;
+
+import domain.User;
+
+import java.util.List;
+
+public interface IUserDao {
+    // create
+    void create(User user);
+
+    // read
+    List<User> getAll();
+
+    User getById(int id);
+
+}
---
+++ src/main/java/dao/implementation/UserDao.java
@@ -0,0 +1,80 @@
+package dao.implementation;
+
+import dao.IUserDao;
+import db.connection.ConnectionBuilder;
+import db.connection.PoolConnection;
+import domain.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDao implements IUserDao {
+    ConnectionBuilder poolConnection = new PoolConnection();
+
+    @Override
+    public void create(User user) {
+
+        String sql = "INSERT INTO USERS (id, first_name, last_name, email, phone, balance, login, password, role_id)" +
+                "VALUES (?,?,?,?,?,?,?,?,?)";
+        try (PreparedStatement preparedStatement = poolConnection.getConnection().prepareStatement(sql)) {
+            preparedStatement.setInt(1, user.getId());
+            preparedStatement.setString(2, user.getFirstName());
+            preparedStatement.setString(3, user.getLastName());
+            preparedStatement.setString(4, user.getEmail());
+            preparedStatement.setString(5, user.getPhone());
+            preparedStatement.setBigDecimal(6, user.getBalance());
+            preparedStatement.setString(7, user.getLogin());
+            preparedStatement.setString(8, user.getPassword());
+            preparedStatement.setInt(9, user.getRoleId());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(" Can't create user into DataBase");
+        }
+    }
+
+    @Override
+    public List<User> getAll() {
+
+        List<User> userList = new ArrayList<>();
+        String sql = "SELECT id, first_name, last_name, email, phone, balance, login, password, role_id ";
+        try (Statement statement = poolConnection.getConnection().createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+            while (resultSet.next()) {
+                userList.add(extractUser(resultSet));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(" Can't get users from DataBase ");
+        }
+        return userList;
+    }
+
+    @Override
+    public User getById(int id) {
+
+        String sql = "SELECT id, first_name, last_name, email, phone, balance, login, password, role_id where id = ?";
+        try (PreparedStatement preparedStatement = poolConnection.getConnection().prepareStatement(sql)) {
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return extractUser(resultSet);
+        } catch (SQLException e) {
+            throw new RuntimeException(" Can't get user by id from DataBase ");
+        }
+
+    }
+    private User extractUser(ResultSet resultSet) throws SQLException {
+        User user = new User();
+        user.setId(resultSet.getInt("id"));
+        user.setFirstName(resultSet.getString("first_name"));
+        user.setLastName(resultSet.getString("last_name"));
+        user.setEmail(resultSet.getString("email"));
+        user.setPhone(resultSet.getString("phone"));
+        user.setBalance(resultSet.getBigDecimal("balance"));
+        user.setLogin(resultSet.getString("login"));
+        user.setPassword(resultSet.getString("password"));
+        user.setRoleId(resultSet.getInt("role_id"));
+        return user;
+    }
+
+}
