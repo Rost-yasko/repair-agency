e73c4657011d725ae644f8de2a68254a9249a4c8
Rostyslav <rostyasko@gmail.com>
2023-03-21 15:08:29 +0200

    add login

--- src/main/java/web/Registration.java
+++ src/main/java/web/Registration.java
@@ -5,13 +5,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+
 
 public class RegistrationServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
-//        PrintWriter printWriter = response.getWriter();
-//        printWriter.println("log in");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
